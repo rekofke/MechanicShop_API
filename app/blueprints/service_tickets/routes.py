7a273851f6c8a23d1b63b99c41799b018dbacfd1
rekofke <rekofke.eric@gmail.com>
2025-04-23 17:21:08 -0700

    uncommented comments in service_tickets/routes.py

--- app/blueprints/service_tickets/routes.py
+++ app/blueprints/service_tickets/routes.py
@@ -21,21 +21,21 @@ def add_ticket():
     if customer:
         new_ticket = Service_Ticket(**ticket_data)   
     # use data to create an instance of service_ticket
-    # new_ticket = Service_Ticket(
-    #     date=ticket_data['date'], 
-    #     type=ticket_data['type'], 
-    #     status=ticket_data['status'],
-    #     customer_id=ticket_data.get('customer_id'),
-    #     vehicle_id=ticket_data.get('vehicle_id'),
-    #     mechanic_id=ticket_data.get('mechanic_id')
-    # )
+    new_ticket = Service_Ticket(
+        date=ticket_data['date'], 
+        type=ticket_data['type'], 
+        status=ticket_data['status'],
+        customer_id=ticket_data.get('customer_id'),
+        vehicle_id=ticket_data.get('vehicle_id'),
+        mechanic_id=ticket_data.get('mechanic_id')
+    )
 
     # add the new ticket to the session
     db.session.add(new_ticket)
     db.session.commit()
     
     return service_ticket_schema.jsonify(new_ticket), 201
-    # return jsonify({'message': 'Customer not found'}), 404
+    return jsonify({'message': 'Customer not found'}), 404
 
 
 # get all tickets
@@ -61,47 +61,47 @@ def get_ticket(service_ticket_id):
     return service_ticket_schema.jsonify(ticket), 200
 
 # # Add mechanic to ticket
-# @service_tickets_bp.route('/<int:ticket_id>/add-mechanic/<int:mechanic_id>', methods=["PUT"])
-# def add_mechanic(ticket_id, mechanic_id):
-#     ticket = db.session.get(Service_Ticket, ticket_id)
-#     mechanic = db.session.get(Mechanic, mechanic_id)
+@service_tickets_bp.route('/<int:ticket_id>/add-mechanic/<int:mechanic_id>', methods=["PUT"])
+def add_mechanic(ticket_id, mechanic_id):
+    ticket = db.session.get(Service_Ticket, ticket_id)
+    mechanic = db.session.get(Mechanic, mechanic_id)
 
-#     if ticket and mechanic:
-#         if mechanic not in ticket.mechanics:
-#             ticket.mechanics.append(mechanic)
-#             db.session.commit()
-#             return jsonify({
-#                 "message": f"successfully added {mechanic.name} to the ticket",
-#                 "ticket": service_ticket_schema.dump(ticket),
-#                 "mechanics": mechanics_schema.dump(ticket.mechanics)
-#             }), 200
+    if ticket and mechanic:
+        if mechanic not in ticket.mechanics:
+            ticket.mechanics.append(mechanic)
+            db.session.commit()
+            return jsonify({
+                "message": f"successfully added {mechanic.name} to the ticket",
+                "ticket": service_ticket_schema.dump(ticket),
+                "mechanics": mechanics_schema.dump(ticket.mechanics)
+            }), 200
         
-#         return jsonify({"error": f"{mechanic.name} is already assigned to this ticket"})
-#     return jsonify({
-#         "message": "Invalid ticket or mechanic ID"
-#     }), 404
+        return jsonify({"error": f"{mechanic.name} is already assigned to this ticket"})
+    return jsonify({
+        "message": "Invalid ticket or mechanic ID"
+    }), 404
     
 
-# # remove mechanic from ticket
-# @service_tickets_bp.route('/<int:ticket_id>/remove-mechanic/<int:mechanic_id>', methods=["PUT"])
-# def remove_mechanic(ticket_id, mechanic_id):
-#     ticket = db.session.get(Service_Ticket, ticket_id)
-#     mechanic = db.session.get(Mechanic, mechanic_id)
+# remove mechanic from ticket
+@service_tickets_bp.route('/<int:ticket_id>/remove-mechanic/<int:mechanic_id>', methods=["PUT"])
+def remove_mechanic(ticket_id, mechanic_id):
+    ticket = db.session.get(Service_Ticket, ticket_id)
+    mechanic = db.session.get(Mechanic, mechanic_id)
 
-#     if ticket and mechanic:
-#         if mechanic in ticket.mechanics:
-#             ticket.mechanics.remove(mechanic)
-#             db.session.commit()
-#             return jsonify({
-#                 "message": f"successfully remove {mechanic.name} from the ticket",
-#                 "ticket": service_ticket_schema.dump(ticket),
-#                 "mechanics": mechanics_schema.dump(ticket.mechanics)
-#             }), 200
+    if ticket and mechanic:
+        if mechanic in ticket.mechanics:
+            ticket.mechanics.remove(mechanic)
+            db.session.commit()
+            return jsonify({
+                "message": f"successfully remove {mechanic.name} from the ticket",
+                "ticket": service_ticket_schema.dump(ticket),
+                "mechanics": mechanics_schema.dump(ticket.mechanics)
+            }), 200
         
-#         return jsonify({"error": f"{mechanic.name} was not assigned to this ticket"}), 400
-#     return jsonify({
-#         "message": "Invalid ticket or mechanic ID"
-#     }), 400
+        return jsonify({"error": f"{mechanic.name} was not assigned to this ticket"}), 400
+    return jsonify({
+        "message": "Invalid ticket or mechanic ID"
+    }), 400
 
 
 # update ticket
