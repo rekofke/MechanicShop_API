6c8e39aff132b531c3f2fd3aacde02628194815c
rekofke <rekofke.eric@gmail.com>
2025-05-07 19:53:51 -0700

    Created endpoint in swagger to delete mechanic, tested endpoint in swagger, sucessful deletion

--- mechanicAPI/app/utils/utils.py
+++ mechanicAPI/app/utils/utils.py
@@ -1,6 +1,4 @@
 from datetime import datetime, timedelta, timezone
-from jose import jwt
-import jose
 from flask import request, jsonify
 from functools import wraps
 import jwt
@@ -26,7 +24,8 @@ def encode_token(mechanic_id):  # using unique pieces of info to make our tokens
 def token_required(f):  # token required decorator: takes (f) usually route handler function as an argument and uses it to check if request has valid token
     @wraps(f)
     def decorated(*args, **kwargs):  # ensures the wrapped function (decorated) retains name and docstring of the original function (f)
-        token = None  # initalizes token to none and checks if authorization header is present in request. if so splits header to extract actual token
+        token = None # initalizes token to none and checks if authorization header is present in request. if so splits header to extract actual token
+        auth_header = request.headers.get("Authorization") 
 
         #look for token in the request headers
         if "Authorization" in request.headers:
@@ -39,12 +38,12 @@ def token_required(f):  # token required decorator: takes (f) usually route hand
                 # Decode the token
                 data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
                 request.mechanic_id = int(data['sub']) # Fetch the mechanic id
-                print(data)
+                print(f"decoded token data: {data}")
                 
 
-            except jose.ExpiredSignatureError:  # if token has expired or invalid caches exceptions and returns corresponding error message
+            except jwt.ExpiredSignatureError:  # if token has expired or invalid caches exceptions and returns corresponding error message
                 return jsonify({"message": "Token has expired!"}), 401
-            except jose.exceptions.JWTError:  # if token is invalid or malformed caches exceptions and returns corresponding error message
+            except jwt.InvalidTokenError as e: # if token is invalid or malformed caches exceptions and returns corresponding error message
                 return jsonify({"message": "Invalid token!"}), 401
             
             return f(*args, **kwargs)  # if token is valid calls original function (f) passing the userid and any other args
